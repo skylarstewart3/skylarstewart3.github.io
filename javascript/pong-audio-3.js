ef0253dbdd5ca4947b6c6dd628373bddd024f2c6
skylarstewart3 <85413521+skylarstewart3@users.noreply.github.com>
2021-06-15 18:03:03 -0500

    Update pong-audio-3.js

--- javascript/pong-audio-3.js
+++ javascript/pong-audio-3.js
@@ -43,13 +43,13 @@ export var soundArray = []; //list of sounds loaded
 
 //Here is where all the Sound File Players Start
 
-export var wallSound = new soundFile("tap-percussive-3.mp3"); //load sound
+export var wallSound = new soundFile("haunting.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
 export var paddleSound = new soundFile("tap-resonant-3.mp3");
 soundArray.push(paddleSound);
 
-export var scoreSound = new soundFile("ocean-waves-3.mp3");
+export var scoreSound = new soundFile("haunting.mp3");
 soundArray.push(scoreSound);
 
 export var ambientSound = new soundFile("fireplace.mp3");
