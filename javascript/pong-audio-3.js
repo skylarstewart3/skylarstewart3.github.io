c5e533786b21300d1ee845871b39bde7297957b3
skylarstewart3 <85413521+skylarstewart3@users.noreply.github.com>
2021-06-24 15:54:04 -0500

    Update pong-audio-3.js

--- javascript/pong-audio-3.js
+++ javascript/pong-audio-3.js
@@ -46,7 +46,7 @@ export var soundArray = []; //list of sounds loaded
 export var wallSound = new soundFile("page-turn.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
-export var paddleSound = new soundFile("haunting2.mp3");
+export var paddleSound = new soundFile("paddle.mp3");
 soundArray.push(paddleSound);
 
 export var scoreSound = new soundFile("ambiance.mp3");
