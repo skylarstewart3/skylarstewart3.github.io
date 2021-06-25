feb93306a392309dc266d602008ab7a4bdc3501e
skylarstewart3 <85413521+skylarstewart3@users.noreply.github.com>
2021-06-25 15:30:39 -0500

    Update pong-audio-3.js

--- javascript/pong-audio-3.js
+++ javascript/pong-audio-3.js
@@ -55,7 +55,7 @@ soundArray.push(scoreSound);
 export var ambientSound = new soundFile("background1.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = -5; //turn down volume
+ambientSound.player.volume.value = -7; //turn down volume
 
 export var adventureMusic = new soundFile("whisper.mp3");
 soundArray.push(adventureMusic);
@@ -65,4 +65,4 @@ adventureMusic.player.volume.value = -10;
 export var villageMusic = new soundFile("fireplace.mp3");
 soundArray.push(villageMusic);
 villageMusic.player.loop = true;
-villageMusic.player.volume.value = -16;
+villageMusic.player.volume.value = -15;
